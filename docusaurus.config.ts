bfae0a0434d6fdcbd777d309a329e0839a51c5c6
shihu <miraculous0218@hotmail.com>
2025-08-12 15:29:34 +0800

    modify config file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Personal Website',
+  tagline: 'My personal site built with Docusaurus',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -15,18 +15,19 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/ishitoralin',
+  url: 'https://ishitoralin.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Personal-Website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ishitora lin', // Usually your GitHub org/user name.
-  projectName: 'Personal-Website', // Usually your repo name.
-  deploymentBranch: 'gh-pages',  // GitHub Pages 預設分支
-  trailingSlash: false,
+  organizationName: 'ishitoralin',     // 你的 GitHub 帳號
+  projectName: 'Personal-Website',     // 你的 repo 名稱
+  deploymentBranch: 'gh-pages',        // 部署分支
   
+  trailingSlash: false,
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
